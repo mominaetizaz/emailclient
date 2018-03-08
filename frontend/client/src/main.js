5f4158d1ea9523671244a0e44237cbdfa34b023a
Momin Aitizaz <mominaetizaz@gmail.com>
2018-03-08 16:24:27 +1100

    Update main.js

--- frontend/client/src/main.js
+++ frontend/client/src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import Vuelidate from 'vuelidate'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
 import App from './App'
 import router from './router'
 
