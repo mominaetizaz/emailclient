1d640891cf0705562689f7799457ee5a0728720d
Momin Aitizaz <mominaetizaz@gmail.com>
2018-03-12 23:30:57 +1100

    Update sendGrid.js

--- backend/src/emailProvider/sendGrid.js
+++ backend/src/emailProvider/sendGrid.js
@@ -2,21 +2,12 @@
 
 //this is the official sendgrid node plugin to use their api
 const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 class SendGrid {
- sgMail.setApiKey(process.env.SENDGRIDKEY);
-
- sendEmail(message) {
-  let isValid = validate(message);
-
-   if (isValid) {
-     return sgMail.send(message);
-   }
- }
-
- validate(message) {
-
- }
+    sendEmail(message) {
+        return sgMail.send(message);
+    }
 }
 
 module.exports = SendGrid;
