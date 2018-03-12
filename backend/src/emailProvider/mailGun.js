4c04f51f6826636946a0e285cbab229ba49bd874
Momin Aitizaz <mominaetizaz@gmail.com>
2018-03-12 15:34:33 +1100

    WIP

--- backend/src/emailProvider/mailGun.js
+++ backend/src/emailProvider/mailGun.js
@@ -1,1 +1,21 @@
+const axios = require('axios');
 
+sendEmail(message) {
+  let isValid = validate(message);
+  
+  if (isValid) {
+    return axios({
+      method: 'post',
+      url: 'https://api.mailgun.net/v3/sandbox171bb31efeaf4e2db87f13c9db671e61.mailgun.org/messages',
+      auth: {
+          username: 'api',
+          password: process.env.MAILGUNKEY
+      },
+      params: msg
+    });
+  }
+}
+
+validate(message) {
+  
+}
