ea4f0cf86af754cc7f830aef1513f508b1ddb1da
Momin Aitizaz <mominaetizaz@gmail.com>
2018-03-12 15:44:09 +1100

    Update mailGun.js

--- backend/src/emailProvider/mailGun.js
+++ backend/src/emailProvider/mailGun.js
@@ -1,21 +1,28 @@
+'use strict';
 const axios = require('axios');
 
-sendEmail(message) {
-  let isValid = validate(message);
+class MailGun {
   
-  if (isValid) {
-    return axios({
-      method: 'post',
-      url: 'https://api.mailgun.net/v3/sandbox171bb31efeaf4e2db87f13c9db671e61.mailgun.org/messages',
-      auth: {
-          username: 'api',
-          password: process.env.MAILGUNKEY
-      },
-      params: msg
-    });
+  sendEmail(message) {
+    let isValid = validate(message);
+
+    if (isValid) {
+      return axios({
+        method: 'post',
+        url: 'https://api.mailgun.net/v3/sandbox171bb31efeaf4e2db87f13c9db671e61.mailgun.org/messages',
+        auth: {
+            username: 'api',
+            password: process.env.MAILGUNKEY
+        },
+        params: msg
+      });
+    }
   }
+
+  validate(message) {
+
+  }
+  
 }
 
-validate(message) {
-  
-}
+module.exports = MailGun;
