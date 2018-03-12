ebb35de42e718858fcd923715d697b4e6aa46f70
Momin Aitizaz <mominaetizaz@gmail.com>
2018-03-12 23:33:31 +1100

    Update mailGun.js

--- backend/src/emailProvider/mailGun.js
+++ backend/src/emailProvider/mailGun.js
@@ -2,22 +2,21 @@
 const axios = require('axios');
 
 class MailGun {
-  
-  sendEmail(message) {
-    message.to.join(',');
-    message.cc.join(',');
-    message.bcc.join(',');
-    
-    return axios({
-      method: 'post',
-      url: 'https://api.mailgun.net/v3/sandbox171bb31efeaf4e2db87f13c9db671e61.mailgun.org/messages',
-      auth: {
-          username: 'api',
-          password: process.env.MAILGUNKEY
-      },
-      params: message
-    });
-  }
+    sendEmail(message) {
+        message.to.join(',');
+        message.cc.join(',');
+        message.bcc.join(',');
+
+        return axios({
+            method: 'post',
+            url: 'https://api.mailgun.net/v3/sandbox171bb31efeaf4e2db87f13c9db671e61.mailgun.org/messages',
+            auth: {
+                username: 'api',
+                password: process.env.MAILGUN_KEY
+            },
+            params: message
+        });
+    }
 }
 
 module.exports = MailGun;
